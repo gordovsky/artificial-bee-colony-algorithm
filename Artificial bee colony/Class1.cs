e2c1b05ab164ce31f75a3fcccd192d4c2cddf669
Denis Gordovsky <gordovsky.d@gmail.com>
2016-11-09 19:02:09 +0200

    initial commit

---
+++ Artificial bee colony/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artificial_bee_colony
+{
+    class Swarm
+    {
+        Point bestPosition;
+        Agent[] bees; 
+        public Swarm()
+        {
+
+        }
+    }
+    class Point
+    {
+        int x;
+        int y;
+        public Point() { }
+    }
+    class Agent
+    {
+        double velocity;
+        Point personalBestPostition;
+        //Point globalBestPositiob;
+        public Agent()
+        {
+
+        }
+        private void recalculateVelocity()
+        {
+
+        }
+    }
+}
---
+++ Artificial bee colony/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artificial_bee_colony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
